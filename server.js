2fa82d6ba6db06c0020317c7dfe82e181d7d9d70
aitsereal1973 <odidoquinom@hotmail.com>
2025-09-07 14:32:19 -0500

    Updated MongoDB connection string to use environment variables for host and database name

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 // Conexión a la base de datos
-mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:27017/aplicacion-de-recetas?retryWrites=true&ssl=true`, {
+mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST || 'localhost'}:${process.env.DB_PORT || '27017'}/${process.env.DB_NAME || 'aplicacion-de-recetas'}?retryWrites=true&ssl=true`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {

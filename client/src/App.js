a4d2a80d28af64deda6f4be9737f9445bb50b3e8
aitsereal1973 <odidoquinom@hotmail.com>
2025-09-07 14:27:59 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Bienvenido a Aplicación de Recetas de Cocina</Text>
+    </View>
+  );
+};
+
+export default App;
